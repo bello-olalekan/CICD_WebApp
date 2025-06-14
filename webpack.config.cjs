510c8da1a040f3b9af4748e2e51e7f8c4c95861d
unknown <belloolalekan11@yahoo.com>
2025-06-14 21:54:45 +0100

    index.css

---
+++ webpack.config.cjs
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true, // Clean dist folder before each build
+  },
+  mode: 'development', // Change to 'production' for production builds
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // Make sure you have this file
+      filename: 'index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  }
+};
