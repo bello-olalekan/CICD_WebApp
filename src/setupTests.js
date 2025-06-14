6e3fc5aa6cc5fd8dfd2500e976a2d760eaacfb40
unknown <belloolalekan11@yahoo.com>
2025-06-14 19:55:38 +0100

    updated src folder file

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,4 @@
 
 // src/setupTests.js
 import '@testing-library/jest-dom'; // Example setup for React Testing Library
-
 // Add any global setup for your tests here
-// src/setupTests.js
-{
-  "setupFilesAfterEnv": ["<rootDir>/src/setupTests.js"]
-}
